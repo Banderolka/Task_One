e93ad0ace80a747303bcd617e328550278734fac
Banderolka <bothisava@gmail.com>
2022-09-18 21:10:51 +0300

    Initial commit

---
+++ src/TaskOne.kt
@@ -0,0 +1,47 @@
+fun main(args: Array<String>){
+    /*
+    Задание № 1
+     */
+
+    var anyNull: Any? = null
+    anyNull = 5
+    var number = anyNull/2.5 // получмлась переменная типа Double
+
+    /*
+    Задание № 2
+     */
+
+    var helloString = "hello"
+    var aplanaString = "aplana"
+    println(helloString + " " + aplanaString)
+
+    /*
+    Задание № 3
+     */
+
+    var five: Char = '5'
+    var fiveString: String = five.toString()
+    var fiveByte: Byte = five.toByte()
+    var fiveShort: Short = five.toShort()
+    var fiveInt: Int = five.toInt()
+    var fiveDouble: Double = five.toDouble()
+    var fiveFloat: Float = five.toFloat()
+    var fiveLong: Long = five.toLong()
+
+    /*
+    Задание № 4
+     */
+
+    println("Нипишите ваше имя и нажмите Enter")
+    var userName: String? = readLine()
+    var stringName: String = userName.toString()
+    println("Приветствую $stringName")
+
+    /*
+    Задание № 5
+     */
+
+    var range= "a" .. "d"
+    println(range)
+
+}
